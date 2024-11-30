14d2f94e69da256cb252d922b2a8a34138ce5309
Erwann Jouvet <vinistys@gmail.com>
2024-11-30 03:02:15 +0100

    feature: CRUD Users

--- backend/start/routes.ts
+++ backend/start/routes.ts
@@ -6,3 +6,13 @@
 | The routes file is used for defining the HTTP routes.
 |
 */
+import UsersController from '#controllers/http/users_controllers'
+import router from '@adonisjs/core/services/router'
+
+router.group(() => {
+  router.get('users', [UsersController, 'index'])
+  router.post('users', [UsersController, 'store'])
+  router.get('users/:id', [UsersController, 'show'])
+  router.put('users/:id', [UsersController, 'update'])
+  router.delete('users/:id', [UsersController, 'destroy'])
+}).prefix('/api')
