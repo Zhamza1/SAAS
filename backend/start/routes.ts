b3df1c6ece97d4c6d54081fa9b406a3bc64b3580
Hamza Mahmood <hamza.mahmood@outlook.fr>
2025-01-07 21:04:30 +0100

    add frontend login and register

--- backend/start/routes.ts
+++ backend/start/routes.ts
@@ -6,25 +6,28 @@
 | The routes file is used for defining the HTTP routes.
 |
 */
-import UsersController from '#controllers/http/users_controllers';
-import AuthController from '#controllers/http/authController';
-import AuthMiddleware from '#middleware/authMiddleware';
-import router from '@adonisjs/core/services/router';
+import UsersController from '#controllers/http/users_controllers'
+import AuthController from '#controllers/http/authController'
+import AuthMiddleware from '#middleware/authMiddleware'
+import router from '@adonisjs/core/services/router'
 
-const authMiddleware = new AuthMiddleware();
+const authMiddleware = new AuthMiddleware()
 
-router.group(() => {
-  router.get('users', [UsersController, 'index'])
-  router.post('users', [UsersController, 'store'])
-  router.get('users/:id', [UsersController, 'show'])
-  router.put('users/:id', [UsersController, 'update'])
-  router.delete('users/:id', [UsersController, 'destroy'])
-}).prefix('/api')
-// .use([AuthMiddleware])
-.use([authMiddleware.handle.bind(authMiddleware)]);
+router
+  .group(() => {
+    router.get('users', [UsersController, 'index'])
+    router.post('users', [UsersController, 'store'])
+    router.get('users/:id', [UsersController, 'show'])
+    router.put('users/:id', [UsersController, 'update'])
+    router.delete('users/:id', [UsersController, 'destroy'])
+  })
+  .prefix('/api/')
+  // .use([AuthMiddleware])
+  .use([authMiddleware.handle.bind(authMiddleware)])
 
-
-router.group(() => {
-  router.post('register', [AuthController, 'register'])
-  router.post('login', [AuthController, 'login'])
-}).prefix('/api/auth');
+router
+  .group(() => {
+    router.post('register', [AuthController, 'register'])
+    router.post('login', [AuthController, 'login'])
+  })
+  .prefix('/api/auth/')
